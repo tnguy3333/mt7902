9344af8c79088bbef2a928d5f1b6d76f6d055db1
tnguy3333 <tnguy3333@gmail.com>
2025-07-04 20:37:09 -0700

    reorganize

--- load.sh
+++ load.sh
@@ -1,14 +1,20 @@
+#!/bin/bash
+
 sudo dmesg -c
 sudo rmmod mt7902e
 sudo rmmod mt792x-lib
 sudo rmmod mt76-connac-lib
 sudo rmmod mt76
 
-make clean
-make
+(
+    cd src
 
-sudo modprobe mac80211
-sudo insmod mt76.ko
-sudo insmod mt76-connac-lib.ko
-sudo insmod mt792x-lib.ko
-sudo insmod mt7902/mt7902e.ko
+    make clean
+    make
+
+    sudo modprobe mac80211
+    sudo insmod mt76.ko
+    sudo insmod mt76-connac-lib.ko
+    sudo insmod mt792x-lib.ko
+    sudo insmod mt7902/mt7902e.ko
+)
