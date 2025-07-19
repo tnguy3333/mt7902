c1c8e6f9fab8de54fbaa5fd733462def05848c0c
tnguy3333 <tnguy3333@gmail.com>
2025-07-19 14:02:24 -0700

    add chip id and firmware paths

--- src/mt7902/pci.c
+++ src/mt7902/pci.c
@@ -14,18 +14,8 @@
 #include "mcu.h"
 
 static const struct pci_device_id mt7921_pci_device_table[] = {
-	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x7961),
-		.driver_data = (kernel_ulong_t)MT7921_FIRMWARE_WM },
-	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x7922),
-		.driver_data = (kernel_ulong_t)MT7922_FIRMWARE_WM },
-	{ PCI_DEVICE(PCI_VENDOR_ID_ITTIM, 0x7922),
-		.driver_data = (kernel_ulong_t)MT7922_FIRMWARE_WM },
-	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x0608),
-		.driver_data = (kernel_ulong_t)MT7921_FIRMWARE_WM },
-	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x0616),
-		.driver_data = (kernel_ulong_t)MT7922_FIRMWARE_WM },
-	{ PCI_DEVICE(PCI_VENDOR_ID_MEDIATEK, 0x7920),
-		.driver_data = (kernel_ulong_t)MT7920_FIRMWARE_WM },
+    { PCI_DEVICE()PCI_VENDOR_ID_MEDIATEK, 0x7902,
+        .driver_data = (kernel_ulong_t)MT7902_FIRMWARE_WM },
 	{ },
 };
 
@@ -573,12 +563,8 @@ static struct pci_driver mt7921_pci_driver = {
 module_pci_driver(mt7921_pci_driver);
 
 MODULE_DEVICE_TABLE(pci, mt7921_pci_device_table);
-MODULE_FIRMWARE(MT7920_FIRMWARE_WM);
-MODULE_FIRMWARE(MT7920_ROM_PATCH);
-MODULE_FIRMWARE(MT7921_FIRMWARE_WM);
-MODULE_FIRMWARE(MT7921_ROM_PATCH);
-MODULE_FIRMWARE(MT7922_FIRMWARE_WM);
-MODULE_FIRMWARE(MT7922_ROM_PATCH);
+MODULE_FIRMWARE(MT7902_FIRMWARE_WM);
+MODULE_FIRMWARE(MT7902_ROM_PATCH);
 MODULE_AUTHOR("Sean Wang <sean.wang@mediatek.com>");
 MODULE_AUTHOR("Lorenzo Bianconi <lorenzo@kernel.org>");
 MODULE_DESCRIPTION("MediaTek MT7921E (PCIe) wireless driver");
